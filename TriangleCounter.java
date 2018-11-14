11f945f838bfcdedf5b48136ff1e787cee02d6ed
roselinapradjanata <roselinapradjanata@gmail.com>
2018-11-14 09:57:27 +0700

    Fix preprocessing

--- TriangleCounter.java
+++ TriangleCounter.java
@@ -18,9 +18,9 @@ import org.apache.hadoop.util.ToolRunner;
 
 public class TriangleCounter extends Configured implements Tool {
 
-	public static class PreprocessingMapper extends Mapper<LongWritable, Text, LongWritable, LongWritable> {
-		LongWritable mKey = new LongWritable();
-		LongWritable mValue = new LongWritable();
+	public static class PreprocessingMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+		Text mKey = new Text();
+		LongWritable zero = new LongWritable(0);
 
 		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 			String line = value.toString();
@@ -29,11 +29,29 @@ public class TriangleCounter extends Configured implements Tool {
 			long u = Long.parseLong(tokenizer.nextToken());
 			long v = Long.parseLong(tokenizer.nextToken());
 
+			mKey.set(u + "," + v);
+			context.write(mKey, zero);
+			
+			mKey.set(v + "," + u);
+			context.write(mKey, zero);
+		}
+	}
+
+	public static class PreprocessingReducer extends Reducer<Text, LongWritable, LongWritable, LongWritable> {
+		LongWritable mKey = new LongWritable();
+		LongWritable mValue = new LongWritable();
+
+		public void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+			String line = key.toString();
+			StringTokenizer tokenizer = new StringTokenizer(line, ",");
+
+			long u = Long.parseLong(tokenizer.nextToken());
+			long v = Long.parseLong(tokenizer.nextToken());
+			
 			mKey.set(u);
 			mValue.set(v);
-			
+
 			context.write(mKey, mValue);
-			context.write(mValue, mKey);
 		}
 	}
 
@@ -91,17 +109,20 @@ public class TriangleCounter extends Configured implements Tool {
 		Job job1 = Job.getInstance(getConf(), "preprocessing");
 		job1.setJarByClass(TriangleCounter.class);
 
-		job1.setMapOutputKeyClass(LongWritable.class);
+		job1.setMapOutputKeyClass(Text.class);
 		job1.setMapOutputValueClass(LongWritable.class);
 
+		job1.setOutputKeyClass(LongWritable.class);
+		job1.setOutputValueClass(LongWritable.class);
+
 		job1.setMapperClass(PreprocessingMapper.class);
-		job1.setNumReduceTasks(0);
+		job1.setReducerClass(PreprocessingReducer.class);
 
 		job1.setInputFormatClass(TextInputFormat.class);
 		job1.setOutputFormatClass(TextOutputFormat.class);
 
 		FileInputFormat.addInputPath(job1, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job1, new Path("nyancat"));
+		FileOutputFormat.setOutputPath(job1, new Path("/user/roselina/temp"));
 
 		Job job2 = Job.getInstance(getConf(), "triads");
 		job2.setJarByClass(TriangleCounter.class);
@@ -118,7 +139,7 @@ public class TriangleCounter extends Configured implements Tool {
 		job2.setInputFormatClass(TextInputFormat.class);
 		job2.setOutputFormatClass(TextOutputFormat.class);
 
-		FileInputFormat.addInputPath(job2, new Path("nyancat"));
+		FileInputFormat.addInputPath(job2, new Path("/user/roselina/temp"));
 		FileOutputFormat.setOutputPath(job2, new Path(args[1]));
 
 		int ret = job1.waitForCompletion(true) ? 0 : 1;
